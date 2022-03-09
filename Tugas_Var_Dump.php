b12fe9e830480690bd82ac113390a8ff2db10069
Blessius321 <blessiussheldo@gmail.com>
2022-03-09 14:48:14 +0700

    Tugas Var Dump

---
+++ Tugas_Var_Dump.php
@@ -0,0 +1,9 @@
+<?php
+
+var_dump(1 + 1);
+var_dump( "1 + 1");
+var_dump(1 + "1");
+var_dump("1" + "1");
+
+echo("Dikarenakan PHP adalah weakly typed language maka tidak ada tipe statis untuk suatu variable. sehingga apabila sebuah string 1 dijumlahkan dengan int 1 hasilnya akan menjadi int 2." );
+?>
